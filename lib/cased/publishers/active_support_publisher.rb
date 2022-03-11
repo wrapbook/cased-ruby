4073d2dc72bc13b9ef8e2e472b266b1cf1611fd6
David Fritsch <dfritsch@wrapbook.com>
2022-03-10 17:57:49 -0700

    Shift conditional require to active support publisher which also needs it

--- lib/cased/publishers/active_support_publisher.rb
+++ lib/cased/publishers/active_support_publisher.rb
@@ -5,6 +5,12 @@ require 'concurrent'
 require 'active_support/notifications'
 require 'cased/publishers/base'
 
+begin
+  require 'active_support/isolated_execution_state'
+rescue LoadError
+  # This is required for ActiveSupport 7.0 but not present in 6.1
+end
+
 module Cased
   module Publishers
     class ActiveSupportPublisher < Base
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,12 +14,6 @@ require 'webmock/minitest'
 require 'mocha/minitest'
 require 'byebug'
 
-begin
-  require 'active_support/isolated_execution_state'
-rescue LoadError
-  # This is required for ActiveSupport 7.0 but not present in 6.1
-end
-
 module Cased
   class Test < Minitest::Test
     def teardown
